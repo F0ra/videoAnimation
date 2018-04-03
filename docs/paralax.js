a0a7d0e1af9a3fbefa19bfb72fcf20d5fae9ed1e
Fora <forexample.fora@gmail.com>
2018-04-03 20:29:34 +0300

    add to docs

--- docs/paralax.js
+++ docs/paralax.js
@@ -27,24 +27,37 @@ let scroller = function(){
         let elem = document.querySelector("#title");
         
         if (window.pageYOffset<800){
+            elem.style.display = "inline-block;";
             elem.style.left=`${window.pageYOffset}px`;
             elem.style.opacity = `${1 -window.pageYOffset/500 }`;
-            elem.style.display = "block;";
         }else{
             elem.style.display = "none;";
         }
-
+        
     }
     let footerAnimation = function(){
         let elem = document.querySelector("footer");
         let opacityStep =  (totalOffsetY - window.pageYOffset)/1000;
         if(scrollLeftInPercentage<9){
+            elem.style.display = "inline-block;";
             elem.style =`opacity: ${0.8-opacityStep}`;
-            
+            // elem.style = "bottom: 0;";
             elem.style.fontSize =`${1+(1*opacityStep)}rem`;
+        }else{
+            elem.style = "bottom: -1000;";
+            elem.style.display = "none;";
         }
     }
-
+    let parallaxSlow = function(){
+        let elem = document.querySelector("#inner_content2");
+        elem.style = `background-position: center +${0.04*window.pageYOffset}px ;`;
+    }
+    let parallaxSlowSlow = function(){
+        let elem = document.querySelector("#inner_content4");
+        elem.style = `background-position: center +${0.01*window.pageYOffset}px ;`;
+    }
+    parallaxSlow();
+    parallaxSlowSlow();
     imageRotation();
     titleAnimation();
     footerAnimation();
